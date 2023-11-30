afc9b7328e15d76801e648529f42974dfa5281bd
qilingzhao <qiling.zhao@qq.com>
2023-11-30 11:07:29 +0800

    temp save

--- ch05/tiled_martix_multi_dynamic_shm.cu
+++ ch05/tiled_martix_multi_dynamic_shm.cu
@@ -12,17 +12,20 @@ __global__
 void matrix_multi_kernel(const Matrix& M, const Matrix& N, Matrix& P, const size_t TileLen) {
     // TODO: check matrix boundary
 
-    int tx = threadIdx.x; int ty = threadIdx.y;
-    int bx = blockIdx.x; int by = blockIdx.y;
-    int gx = bx * TileLen + tx;
-    int gy = by * TileLen + ty;
+    size_t tx = threadIdx.x; size_t ty = threadIdx.y;
+    size_t bx = blockIdx.x; size_t by = blockIdx.y;
+    size_t gx = bx * TileLen + tx;
+    size_t gy = by * TileLen + ty;
 
     extern float shm[];
 
     size_t max_len = max(P.height, P.weight);
-    for (int ph = 0; ph < (max_len + TileLen - 1) / TileLen; ph++) {
+    for (size_t ph = 0; ph < (max_len + TileLen - 1) / TileLen; ph++) {
         // load matrix data from global memory to shm;
-        
+        size_t M_x = ph * TileLen + tx;
+        size_t M_y = ph * TileLen + ty;
+        size_t shm_m_idx = 0;
+
     }
 }
 
